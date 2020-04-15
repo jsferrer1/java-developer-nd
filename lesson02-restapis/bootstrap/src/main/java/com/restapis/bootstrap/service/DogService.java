7638951989fa635b1eeedcb119c0dd6d3c32023a
jsferrer1 <jsferrer1@yahoo.com>
2020-04-15 16:30:01 +0800

    Adds the dog service APIs

---
+++ lesson02-restapis/bootstrap/src/main/java/com/restapis/bootstrap/service/DogService.java
@@ -0,0 +1,12 @@
+package com.restapis.bootstrap.service;
+
+import com.restapis.bootstrap.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
