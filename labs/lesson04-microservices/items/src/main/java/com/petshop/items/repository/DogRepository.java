f837ee82196ae58e64e4bd81de4eb30ecbbbf444
jsferrer1 <jsferrer1@yahoo.com>
2020-04-18 12:57:27 +0800

    Adds the eureka server and items client microservices

---
+++ labs/lesson04-microservices/items/src/main/java/com/petshop/items/repository/DogRepository.java
@@ -0,0 +1,10 @@
+package com.petshop.items.repository;
+
+import com.petshop.items.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
---
+++ labs/part-03/lesson04-microservices/items/src/main/java/com/petshop/items/ItemsApplication.java
@@ -0,0 +1,15 @@
+package com.petshop.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsApplication.class, args);
+	}
+
+}
