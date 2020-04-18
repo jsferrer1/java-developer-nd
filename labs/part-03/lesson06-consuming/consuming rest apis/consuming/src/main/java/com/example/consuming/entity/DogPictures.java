e9887731a1547a63a887a6593a6cc675716bf349
jsferrer1 <jsferrer1@yahoo.com>
2020-04-18 16:14:32 +0800

    Consume a REST API from dog.ceo

---
+++ labs/part-03/lesson06-consuming/consuming rest apis/consuming/src/main/java/com/example/consuming/entity/DogPictures.java
@@ -0,0 +1,29 @@
+package com.example.consuming.entity;
+
+public class DogPictures {
+    private String message;
+    private String status;
+
+    public DogPictures() { }
+
+    public DogPictures(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
